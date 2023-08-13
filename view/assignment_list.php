dc51ceb1379088588c7f0d20a0e055d233b3ec61
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2023-08-13 23:31:47 +0530

    add assignment option

--- view/assignment_list.php
+++ view/assignment_list.php
@@ -49,7 +49,28 @@
     <?php } ?>
 </section>
 
-
+<section id="add" class="add">
+    <h2>Add Assignment</h2>
+    <form action="." method="post" id="add__form" class="add__form">
+        <input type="hidden" name="action" value="add_assignment">
+        <div class="add__inputs">
+            <label>course:</label>
+            <select name="course_id" required>
+                <option value="">Please select</option>
+                <?php foreach ($courses as $course) : ?>
+                    <option value="<?= $course['courseID']; ?>">
+                        <?= $course['courseName']; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <label>Description:</label>
+            <input type="text" name="description" maxlength="120" placeholder="Description" required>
+        </div>
+        <div class="add__addItem">
+            <button class="add-button bold">Add</button>
+        </div>
+    </form>
+</section>
 <br>
 <p><a href=".?action=list_courses">View/Edit Courses</a></p>
 <?php include('view/footer.php') ?>
